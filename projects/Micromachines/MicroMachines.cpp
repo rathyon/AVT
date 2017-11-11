847ddcfb4d3c816280a978a8bbae7ab3f0ced34a
Rathyon <sapphiron7@gmail.com>
2017-11-11 19:37:48 +0000

    Add assimp obj loading

--- projects/Micromachines/MicroMachines.cpp
+++ projects/Micromachines/MicroMachines.cpp
@@ -13,14 +13,21 @@
 #include <math.h>
 #include <iostream>
 #include <sstream>
-
+#include <fstream>
 #include <string>
+#include <map>
 
 // include GLEW to access OpenGL 3.3 functions
 #include <GL/glew.h>
 
 // GLUT is the toolkit to interface with the OS
 #include <GL/freeglut.h>
+
+// assimp include files. These three are usually needed.
+#pragma comment(lib, "assimp.lib") 
+#include "Importer.hpp"	//OO version Header!
+#include "PostProcess.h"
+#include "Scene.h"
 
 // Use Very Simple Libs
 #include "VSShaderlib.h"
@@ -79,9 +86,9 @@ int camera = CHASE;
 
 float top[3];
 float camX, camY, camZ;
-float alpha = 0.0f;
-float beta = 0.0f;
-float r = 5.0f;
+float alpha = 0.0f; // horizontal angle
+float beta = 30.0f; // vertical angle
+float r = 15.0f;
 
 // Mouse Tracking Variables
 int startX, startY, tracking = 0;
@@ -143,7 +150,282 @@ float RNG() {
 	return RNG;
 }
 
-//-----------------------------------------------//
+//--------------------------------------[ ASSIMP ]--------------------------------------//
+
+#define aisgl_min(x,y) (x<y?x:y)
+#define aisgl_max(x,y) (y>x?y:x)
+
+const aiScene* scene = NULL;
+
+struct Mesh {
+
+	GLuint vao;
+	GLuint texIndex;
+	struct Material mat;
+	int numFaces;
+};
+
+Assimp::Importer importer;
+
+float scaleFactor; // why is this needed?! not used anywhere else
+
+std::vector<struct Mesh> meshes; // for storing imported .OBJ files
+
+void set_float4(float f[4], float a, float b, float c, float d){
+	f[0] = a;
+	f[1] = b;
+	f[2] = c;
+	f[3] = d;
+}
+void color4_to_float4(const aiColor4D *c, float f[4]){
+	f[0] = c->r;
+	f[1] = c->g;
+	f[2] = c->b;
+	f[3] = c->a;
+}
+void get_bounding_box_for_node(const aiNode* nd, aiVector3D* min, aiVector3D* max) {
+	aiMatrix4x4 prev;
+	unsigned int n = 0, t;
+
+	for (; n < nd->mNumMeshes; ++n) {
+		const aiMesh* mesh = scene->mMeshes[nd->mMeshes[n]];
+		for (t = 0; t < mesh->mNumVertices; ++t) {
+
+			aiVector3D tmp = mesh->mVertices[t];
+
+			min->x = aisgl_min(min->x, tmp.x);
+			min->y = aisgl_min(min->y, tmp.y);
+			min->z = aisgl_min(min->z, tmp.z);
+
+			max->x = aisgl_max(max->x, tmp.x);
+			max->y = aisgl_max(max->y, tmp.y);
+			max->z = aisgl_max(max->z, tmp.z);
+		}
+	}
+
+	for (n = 0; n < nd->mNumChildren; ++n) {
+		get_bounding_box_for_node(nd->mChildren[n], min, max);
+	}
+}
+void get_bounding_box(aiVector3D* min, aiVector3D* max) {
+
+	min->x = min->y = min->z = 1e10f;
+	max->x = max->y = max->z = -1e10f;
+	get_bounding_box_for_node(scene->mRootNode, min, max);
+}
+bool Import3DFromFile(const std::string& pFile)
+{
+
+	//check if file exists
+	std::ifstream fin(pFile.c_str());
+	if (!fin.fail()) {
+		fin.close();
+	}
+	else {
+		printf("Couldn't open file: %s\n", pFile.c_str());
+		printf("%s\n", importer.GetErrorString());
+		return false;
+	}
+
+	scene = importer.ReadFile(pFile, aiProcessPreset_TargetRealtime_Quality);
+
+	// If the import failed, report it
+	if (!scene)
+	{
+		printf("%s\n", importer.GetErrorString());
+		return false;
+	}
+
+	// Now we can access the file's contents.
+	printf("Import of scene %s succeeded.", pFile.c_str());
+
+	aiVector3D scene_min, scene_max, scene_center;
+	get_bounding_box(&scene_min, &scene_max);
+	float tmp;
+	tmp = scene_max.x - scene_min.x;
+	tmp = scene_max.y - scene_min.y > tmp ? scene_max.y - scene_min.y : tmp;
+	tmp = scene_max.z - scene_min.z > tmp ? scene_max.z - scene_min.z : tmp;
+	scaleFactor = 1.f / tmp;
+
+	// We're done. Everything will be cleaned up by the importer destructor
+	return true;
+}
+void genVAOsAndUniformBuffer(const aiScene *sc) {
+
+	struct Mesh aMesh;
+	struct Material aMat;
+	GLuint buffer;
+
+	// For each mesh
+	for (unsigned int n = 0; n < sc->mNumMeshes; ++n)
+	{
+		const aiMesh* mesh = sc->mMeshes[n];
+
+		// create array with faces
+		// have to convert from Assimp format to array
+		unsigned int *faceArray;
+		faceArray = (unsigned int *)malloc(sizeof(unsigned int) * mesh->mNumFaces * 3);
+		unsigned int faceIndex = 0;
+
+		for (unsigned int t = 0; t < mesh->mNumFaces; ++t) {
+			const aiFace* face = &mesh->mFaces[t];
+
+			memcpy(&faceArray[faceIndex], face->mIndices, 3 * sizeof(unsigned int));
+			faceIndex += 3;
+		}
+		aMesh.numFaces = sc->mMeshes[n]->mNumFaces;
+
+		// generate Vertex Array for mesh
+		glGenVertexArrays(1, &(aMesh.vao));
+		glBindVertexArray(aMesh.vao);
+
+		// buffer for faces
+		glGenBuffers(1, &buffer);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * mesh->mNumFaces * 3, faceArray, GL_STATIC_DRAW);
+
+		// buffer for vertex positions
+		if (mesh->HasPositions()) {
+			glGenBuffers(1, &buffer);
+			glBindBuffer(GL_ARRAY_BUFFER, buffer);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * mesh->mNumVertices, mesh->mVertices, GL_STATIC_DRAW);
+			glEnableVertexAttribArray(VERTEX_COORD_ATTRIB);
+			glVertexAttribPointer(VERTEX_COORD_ATTRIB, 3, GL_FLOAT, 0, 0, 0);
+		}
+
+		// buffer for vertex normals
+		if (mesh->HasNormals()) {
+			glGenBuffers(1, &buffer);
+			glBindBuffer(GL_ARRAY_BUFFER, buffer);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * mesh->mNumVertices, mesh->mNormals, GL_STATIC_DRAW);
+			glEnableVertexAttribArray(NORMAL_ATTRIB);
+			glVertexAttribPointer(NORMAL_ATTRIB, 3, GL_FLOAT, 0, 0, 0);
+		}
+
+		// buffer for vertex texture coordinates
+		if (mesh->HasTextureCoords(0)) {
+			float *texCoords = (float *)malloc(sizeof(float) * 2 * mesh->mNumVertices);
+			for (unsigned int k = 0; k < mesh->mNumVertices; ++k) {
+
+				texCoords[k * 2] = mesh->mTextureCoords[0][k].x;
+				texCoords[k * 2 + 1] = mesh->mTextureCoords[0][k].y;
+
+			}
+			glGenBuffers(1, &buffer);
+			glBindBuffer(GL_ARRAY_BUFFER, buffer);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 2 * mesh->mNumVertices, texCoords, GL_STATIC_DRAW);
+			glEnableVertexAttribArray(TEXTURE_COORD_ATTRIB);
+			glVertexAttribPointer(TEXTURE_COORD_ATTRIB, 2, GL_FLOAT, 0, 0, 0);
+		}
+
+		// unbind buffers
+		glBindVertexArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+		// create material uniform buffer
+		aiMaterial *mtl = sc->mMaterials[mesh->mMaterialIndex];
+		aMat.texCount = 0;
+
+		float c[4];
+		set_float4(c, 0.8f, 0.8f, 0.8f, 1.0f);
+		aiColor4D diffuse;
+		if (AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_DIFFUSE, &diffuse))
+			color4_to_float4(&diffuse, c);
+		memcpy(aMat.diffuse, c, sizeof(c));
+
+		set_float4(c, 0.2f, 0.2f, 0.2f, 1.0f);
+		aiColor4D ambient;
+		if (AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_AMBIENT, &ambient))
+			color4_to_float4(&ambient, c);
+		memcpy(aMat.ambient, c, sizeof(c));
+
+		set_float4(c, 0.0f, 0.0f, 0.0f, 1.0f);
+		aiColor4D specular;
+		if (AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_SPECULAR, &specular))
+			color4_to_float4(&specular, c);
+		memcpy(aMat.specular, c, sizeof(c));
+
+		set_float4(c, 0.0f, 0.0f, 0.0f, 1.0f);
+		aiColor4D emission;
+		if (AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_EMISSIVE, &emission))
+			color4_to_float4(&emission, c);
+		memcpy(aMat.emissive, c, sizeof(c));
+
+		float shininess = 0.0;
+		unsigned int max;
+		aiGetMaterialFloatArray(mtl, AI_MATKEY_SHININESS, &shininess, &max);
+		aMat.shininess = shininess;
+
+		memcpy(aMesh.mat.diffuse, aMat.diffuse, sizeof(aMat.diffuse));
+		memcpy(aMesh.mat.ambient, aMat.ambient, sizeof(aMat.ambient));
+		memcpy(aMesh.mat.specular, aMat.specular, sizeof(aMat.specular));
+		memcpy(aMesh.mat.emissive, aMat.emissive, sizeof(aMat.emissive));
+		aMesh.mat.shininess = aMat.shininess;
+
+		meshes.push_back(aMesh);
+	}
+}
+
+// Render Assimp Model
+
+void renderModel(int model_ID, const aiScene *sc, const aiNode* nd)
+{
+
+	// Get node transformation matrix
+	aiMatrix4x4 m = nd->mTransformation;
+	// OpenGL matrices are column major
+	m.Transpose();
+
+	// save model matrix and apply node transformation
+	pushMatrix(MODEL);
+
+	float aux[16];
+	memcpy(aux, &m, sizeof(float) * 16);
+	multMatrix(mMatrix[MODEL], aux);
+
+	//send matrices
+	computeDerivedMatrix(PROJ_VIEW_MODEL);
+	glUniformMatrix4fv(vm_uniformId, 1, GL_FALSE, mCompMatrix[VIEW_MODEL]);
+	glUniformMatrix4fv(pvm_uniformId, 1, GL_FALSE, mCompMatrix[PROJ_VIEW_MODEL]);
+	computeNormalMatrix3x3();
+	glUniformMatrix3fv(normal_uniformId, 1, GL_FALSE, mNormal3x3);
+
+
+	// draw all meshes assigned to this node
+	for (unsigned int n = 0; n < nd->mNumMeshes; ++n) {
+
+		GLint loc;
+
+		// send materials
+		loc = glGetUniformLocation(pid, "mat.ambient");
+		glUniform4fv(loc, 1, meshes[model_ID].mat.ambient);
+		loc = glGetUniformLocation(pid, "mat.diffuse");
+		glUniform4fv(loc, 1, meshes[model_ID].mat.diffuse);
+		loc = glGetUniformLocation(pid, "mat.specular");
+		glUniform4fv(loc, 1, meshes[model_ID].mat.specular);
+		loc = glGetUniformLocation(pid, "mat.shininess");
+		glUniform1f(loc, meshes[model_ID].mat.shininess);
+
+		// bind texture
+		//glBindTexture(GL_TEXTURE_2D, meshes[nd->mMeshes[n]].texIndex);
+		// bind VAO
+		glBindVertexArray(meshes[nd->mMeshes[n]].vao);
+		// draw
+		glDrawElements(GL_TRIANGLES, meshes[nd->mMeshes[n]].numFaces * 3, GL_UNSIGNED_INT, 0);
+
+	}
+
+	// draw all children
+	for (unsigned int n = 0; n < nd->mNumChildren; ++n) {
+		renderModel(0, sc, nd->mChildren[n]);
+	}
+	popMatrix(MODEL);
+}
+
+//--------------------------------------------------------------------------------------//
+//------------------------------------[ ASSIMP END ]------------------------------------//
+//--------------------------------------------------------------------------------------//
 
 void timer(int value)
 {
@@ -401,6 +683,8 @@ void renderScene(void) {
 		translate(MODEL, orangePos[i][0], orangePos[i][1], orangePos[i][2]);
 		render();
 	}
+
+	renderModel(0, scene, scene->mRootNode);
 
 
 	popMatrix(MODEL);
@@ -684,6 +968,13 @@ void init()
 
 	loadMaterials(ambOrange, diffOrange, specOrange, null, shininess, texCount);
 	createSphere(1.0f, 30);
+
+	/*----------------- ASSIMP -------------------*/
+
+	if (!Import3DFromFile("bench.obj"))
+		std::cout << "ERROR LOADING MODEL" << std::endl;
+
+	genVAOsAndUniformBuffer(scene);
 
 	// some GL settings
 	glEnable(GL_DEPTH_TEST);
