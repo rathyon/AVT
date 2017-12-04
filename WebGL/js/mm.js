0de5ab627834590601b7ddee09fb2660c7bce738
mirrormove <dsilva1295@gmail.com>
2017-12-04 18:13:38 +0000

    Trying out bump mapping

--- WebGL/js/mm.js
+++ WebGL/js/mm.js
@@ -8,62 +8,66 @@ var scene,
 
 var geo, mat, plane;
 
+//Directional Light
 var directional_light = new THREE.DirectionalLight(0xffffff, 0.5);
-var light = new THREE.PointLight( new THREE.Color("rgb(255,70,3)"), 2.5);
+directional_light.position.set(0, 100, 0);
+
+//Point Light
+var light = new THREE.PointLight( new THREE.Color("rgb(255,255,255)"), 2.5);
+light.position.y = 100;
+light.position.x = 100;
+
 
 geo = new THREE.PlaneGeometry(50,50);
 mat = new THREE.MeshPhongMaterial({color: 0x00ffff, specular: 0xffffff, shininess: 5000});
 
 mat = new THREE.MeshBasicMaterial({color: 0x00ffff});
 
-/*var manager = new THREE.LoadingManager();
 var loader = new THREE.TextureLoader();
 
-var bmap =  loader.load("js/textures/normal.tga");
-var map = loader.load("js/textures/stone.tga");
+var bmap = loader.load('js/textures/normal.png');
+var map = loader.load('js/textures/normal.png');
 
 var oldMaterial = new THREE.MeshPhongMaterial({
   //color      :  new THREE.Color("rgb(200,200,200)"),
-  color        : 0xffffff,
+  color        : 0x666666,
   //emissive   :  new THREE.Color("rgb(200,200,200)"),
   //specular   :  new THREE.Color("rgb(200,200,200)"),
   shininess  :  20,
-  //bumpMap    :  bmap,
+  bumpMap    :  bmap,
   //map        :  map,
-  bumpScale  :  0.45,
+  //bumpScale  :  0.45,
 });
-*/
 
-plane = new THREE.Mesh(geo, mat);
+plane = new THREE.Mesh(geo, oldMaterial);
 
 var init = function() {
 	scene = new THREE.Scene();
+	
+	//Window
     renderer = new THREE.WebGLRenderer();
-
-    camera = new THREE.PerspectiveCamera(45, 640/480, 0.1, 1000);
-    camera.position.x = 50;
-    camera.position.y = 50;
-    camera.position.z = 50;
-    camera.lookAt(new THREE.Vector3(0,0,0));
-
     renderer.setSize(640, 480);
+	document.body.appendChild( renderer.domElement );
+	
+	//Camera
+    camera = new THREE.PerspectiveCamera(45, 640/480, 0.1, 1000);
+    camera.position.set(50, 50, 50);
+    camera.lookAt(new THREE.Vector3(0,0,0));
+	scene.add(camera);
+	
+	//Point light
+	scene.add(light);
 
-    scene.add(camera);
-    document.body.appendChild( renderer.domElement );
-
+	//Axis Helper
     scene.add(new THREE.AxisHelper(10));
-
-	directional_light.position.set(0, 100, 0);
+	
 	//scene.add(directional_light);
 
-    scene.add(light);
-    light.position.y = 100;
-    light.position.x = 100;
-
+	//Ground
     scene.add(plane);
-
     plane.rotateOnAxis(new THREE.Vector3(1,0,0), THREE.Math.degToRad(-90));
 
+	//Mouse Controls
     controls = new THREE.OrbitControls( camera, renderer.domElement );
     controls.addEventListener( 'change', render );
 
